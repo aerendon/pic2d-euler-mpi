89b23657071c282b297dff0223e4867363e3139c
Alejandro E. Rendon <alejorendon@utp.edu.co>
2016-03-22 11:17:08 -0500

    Secuential Finished

---
+++ README.md
@@ -0,0 +1,15 @@
+# pic2d-euler
+PIC 2D before Leap-Frog.
+
+COMPILATION REQUIREMENTS
+g++, fftw3, fftw3-dev library (A C subroutine library for computing the discrete Fourier transform).
+
+CUDA version
+nvcc
+
+OpenCL version
+opencl-header, libclc and the OpenCL implementation of the fabricant of the device
+
+Compile with:
+
+make
---
+++ secuential/pic_leap.cpp
@@ -0,0 +1,213 @@
+#include "pic.cpp"
+#include <mpi.h>
+#include <unistd.h> //Hostname
+
+using namespace std;
+using namespace pic;
+
+//********
+
+int main(int argc, char* argv[]) {
+
+	//MPI
+  MPI_Init (&argc, &argv);
+	
+	int rank, size_mpi;
+	char hostname[256];
+	MPI_Comm_rank (MPI_COMM_WORLD, &rank);        //MPI: get current process id 
+	MPI_Comm_size (MPI_COMM_WORLD, &size_mpi);    //MPI: get number of processes 
+	gethostname(hostname,255);
+	printf( "Hello world from %s  process %d of %d\n", hostname, rank, size_mpi);
+
+  //************************
+  // Parámetros del sistema
+  //************************
+  clock_t tiempo0  =  clock();
+
+  int le = 0, li = 0;
+  double  t_0, x_0;
+  int  total_e_perdidos = 0;
+  int  total_i_perdidos = 0;
+  double  mv2perdidas = 0;
+
+  double  ND = NE03D * pow(LAMBDA_D,3);                          //Parámetro del plasma
+  int     k_MAX_inj;   //Tiempo máximo de inyección
+  int     K_total;     //Tiempo total
+  int     Ntv = 8;
+  int     NTSPe, NTSPI, MAX_SPE_dt, MAX_SPI_dt;
+  double  phi0 = 2. * K_BOLTZMANN * Te / (M_PI * E_CHARGE ), E0 = phi0 / X0;
+ // FILE    *outEnergia;
+
+
+  //***************************
+  //Constantes de normalización
+  //***************************
+
+  //double  X0 = LAMBDA_D;                //Escala de longitud: Longitud de Debye
+  double  n0  =  double(NTe) / (X0 * X0 * X0);
+  double  ni0_3D  =  NI03D * pow(X0, 3);
+  double  ne0_3D  =  NE03D * pow(X0, 3);
+  double  om_p  =  VFLUX_E_X / LAMBDA_D;                    //Frecuencia del plasma
+  double hx;
+  //int seed  =  time (NULL);
+  //srand (seed);  // Semilla para generar números aleatorios dependiendo del reloj interno.
+  //******************
+  //ARCHIVOS DE SALIDA
+  //******************
+  //outEnergia = fopen("Energia","w");
+  char buffer[40];
+
+  //****************************************
+  // Inicialización de variables del sistema
+  //****************************************
+  int size = MAX_SPE * sizeof(double);
+  int size1 = J_X * J_Y * sizeof(double);
+  int size2 = J_X * J_Y * sizeof(complex<double>);
+
+  double *pos_e_x, *pos_e_y, *pos_i_x, *pos_i_y, *vel_e_x, *vel_e_y, *vel_i_x, *vel_i_y, *ne, *ni;
+  double *phi, *E_X, *E_Y;
+  complex<double> *rho;
+  //double  E_i,E_e,E_field,E_total,E_perdida;
+
+  pos_e_x = (double *) malloc(size);
+  pos_e_y = (double *) malloc(size);
+  pos_i_x = (double *) malloc(size);
+  pos_i_y = (double *) malloc(size);
+
+  vel_e_x = (double *) malloc(size);
+  vel_e_y = (double *) malloc(size);
+  vel_i_x = (double *) malloc(size);
+  vel_i_y = (double *) malloc(size);
+  ne    = (double *) malloc(size1);
+  ni    = (double *) malloc(size1);
+  E_X   = (double *) malloc(size1);
+  E_Y   = (double *) malloc(size1);
+  phi   = (double *) malloc(size1);
+  rho   = (complex<double> *) malloc(size2);
+
+  //***************************
+  // Normalización de variables
+  //***************************
+
+  t_0 = 1;
+  x_0 = 1;
+  hx = DELTA_X / X0;                            // Paso espacial
+  NTSPe = NTe / FACTOR_CARGA_E;
+  NTSPI = NTI / FACTOR_CARGA_I; // Número total de superpartículas
+  // Inyectadas en un tiempo T0.
+  // ( =  número de superpartículas
+  // Inyectadas por unidad de tiempo,
+  // puesto que T0*(normalizado) = 1.
+
+  int Kemision = 20;  //Pasos para liberar partículas
+  double dt_emision = Kemision * DT; //Tiempo para liberar partículas
+
+  MAX_SPE_dt = NTSPe * dt_emision;   //Número de Superpartículas el. liberadas cada vez.
+  MAX_SPI_dt = MAX_SPE_dt;
+
+
+  // Ciclo de tiempo
+
+  k_MAX_inj = t_0 / DT;
+  K_total = Ntv * k_MAX_inj;
+
+
+  if (rank == 0) {
+    initialize_Particles (pos_e_x, pos_e_y, vel_e_x, vel_e_y, le, FE_MAXWELL_X, FE_MAXWELL_Y, VPHI_E_X, VPHI_E_Y);//Velocidades y posiciones iniciales de las partículas>
+  }
+  else if (rank == 1) {
+    initialize_Particles (pos_i_x, pos_i_y, vel_i_x, vel_i_y, li, FI_MAXWELL_X, FI_MAXWELL_Y, VPHI_I_X, VPHI_I_Y);//Velocidades y posiciones iniciales de las partículas>
+    rank = 0;
+  }
+
+  cout << rank << endl;
+
+  double tacum = 0;
+  for(int kk  =  0, kt  =  0; kt <= K_total; kt++) {
+    rank = 0;
+    /*if(kt % 50000 == 0) {
+      printf("kt = %d\n", kt);
+      printf("le = %d   li = %d \n",le, li );
+    }*/
+    if(kt <= k_MAX_inj && kt == kk) {// Inyectar superpartículas (i-e)
+      le+= MAX_SPE_dt;
+      li+= MAX_SPI_dt;
+      kk = kk + Kemision;
+    }
+    //-----------------------------------------------
+    // Calculo de "densidad de carga 2D del plasma"
+
+    // To MPI
+    if (rank == 0){
+      Concentration (pos_e_x, pos_e_y, ne, le, hx);// Calcular concentración de superpartículas electrónicas
+    }
+    else if (rank == 1) {
+      Concentration (pos_i_x, pos_i_y, ni, li, hx);// Calcular concentración de superpartículas Iónicas
+      rank = 0;
+    }
+
+    for (int j  =  0; j < J_X; j++)
+      for (int k  =  0; k < J_Y; k++)
+        rho[j * J_Y + k] = cte_rho * FACTOR_CARGA_E * (ni[j * J_Y + k] - ne[j * J_Y + k]) / n_0;
+
+    // Calcular potencial eléctrico en puntos de malla
+    poisson2D_dirichletX_periodicY(phi, rho, hx);
+    // Calcular campo eléctrico en puntos de malla
+
+    electric_field(phi, E_X, E_Y, hx);
+
+    // imprimir el potencial electroestatico.
+    if(kt % 10000  ==  0) {
+      sprintf(buffer,"Poisson%d.data", kt);
+      ofstream dataFile(buffer);
+      for (int j  =  0; j < J_X; j++) {
+        double thisx  =  j * hx;
+        for (int k  =  0; k < J_Y; k++) {
+          double thisy  =  k * hx;
+          dataFile << thisx << '\t' << thisy << '\t' << phi[(j * J_Y) + k] << '\n';
+        }
+        dataFile << '\n';
+      }
+      dataFile.close();
+    }
+
+    // Avanzar posiciones de superpartículas electrónicas e Iónicas
+
+    // To MPI
+    if (rank == 0) {
+      Motion(pos_e_x, pos_e_y, vel_e_x, vel_e_y, le, ELECTRONS, E_X, E_Y, hx, total_e_perdidos, mv2perdidas);//, total_elec_perdidos, total_ion_perdidos, mv2_perdidas);
+    }
+    else if (rank == 1) {
+      Motion(pos_i_x, pos_i_y, vel_i_x, vel_i_y, li, IONS, E_X, E_Y, hx, total_i_perdidos, mv2perdidas);//, total_elec_perdidos, total_ion_perdidos, mv2_perdidas);
+      rank = 0;
+    }
+
+    clock_t tiempo1  =  clock();
+    
+      if(kt % 5000 == 0) {
+        if (hostname[0] == 'h') cout << " CPU time " << kt / 5000 << " from " << hostname << "  =  " << double(tiempo1 - tiempo0) / CLOCKS_PER_SEC << " sec" << endl;
+        else cout << hostname << endl;
+        // cout << " CPU time " << kt / 5000 <<  "  =  " << double(tiempo1 - tiempo0) / CLOCKS_PER_SEC << " sec" << endl;
+        tiempo0  =  clock();
+      }
+  } //Cierre del ciclo principal
+
+  free(pos_e_x);
+  free(pos_e_y);
+  free(pos_i_x);
+  free(pos_i_y);
+  free(vel_e_x);
+  free(vel_e_y);
+  free(vel_i_x);
+  free(vel_i_y);
+  free(ne);
+  free(ni);
+  free(phi);
+  free(E_X);
+  free(E_Y);
+  free(rho);
+	
+  MPI_Finalize();  /*Clone MPI*/
+  return (0);
+}// FINAL MAIN
+
