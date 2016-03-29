3b4ed5e03d4f37b79cd8e7ba79365e96db11e1ad
Alejandro E. Rendon <alejorendon@utp.edu.co>
2016-03-29 10:57:54 -0500

    More MPI marks

--- cuda/pic_cuda.cu
+++ cuda/pic_cuda.cu
@@ -419,12 +419,17 @@ namespace pic_cuda {
       int k = (blockIdx.y * blockDim.y + threadIdx.y);
       if(j < J_X - 1 && k < J_Y) {
         //To MPI
-        d_E_X[j * J_Y + k] = (d_phi[(j - 1) * J_Y + k] - d_phi[(j + 1) * J_Y + k]) / (2. * hx);
-        d_E_Y[j * J_Y + k] = (d_phi[j * J_Y + ((J_Y + k - 1) % J_Y)] - d_phi[j * J_Y + ((k + 1) % J_Y)]) / (2. * hx);
-        d_E_X[k] = 0.0; //Cero en las fronteras X
-        d_E_Y[k] = 0.0;
-        d_E_X[(J_X - 1) * J_Y + k] = 0.0;
-        d_E_Y[(J_X - 1) * J_Y + k] = 0.0;
+        if (rank == 0) {
+          d_E_X[j * J_Y + k] = (d_phi[(j - 1) * J_Y + k] - d_phi[(j + 1) * J_Y + k]) / (2. * hx);
+          d_E_X[k] = 0.0; //Cero en las fronteras X
+          d_E_X[(J_X - 1) * J_Y + k] = 0.0;
+        }
+        else if (rank == 1) {
+          d_E_Y[j * J_Y + k] = (d_phi[j * J_Y + ((J_Y + k - 1) % J_Y)] - d_phi[j * J_Y + ((k + 1) % J_Y)]) / (2. * hx);
+          d_E_Y[k] = 0.0;
+          d_E_Y[(J_X - 1) * J_Y + k] = 0.0;
+          rank == 0;
+        }
       }
     }
 
@@ -433,10 +438,15 @@ namespace pic_cuda {
       int k = (blockIdx.x * blockDim.x + threadIdx.x);
       if(k < J_Y) {
         //To MPI
-        d_E_X[k] = 0.0; //Cero en las fronteras X
-        d_E_Y[k] = 0.0;
-        d_E_X[(J_X - 1) * J_Y + k] = 0.0;
-        d_E_Y[(J_X - 1) * J_Y + k] = 0.0;
+        if (rank == 0) {
+          d_E_X[k] = 0.0; //Cero en las fronteras X
+          d_E_X[(J_X - 1) * J_Y + k] = 0.0;
+        }
+        else if (rank == 1) {
+          d_E_Y[k] = 0.0;
+          d_E_Y[(J_X - 1) * J_Y + k] = 0.0;
+          rank = 0;
+        }
       }
     }
 
@@ -481,21 +491,31 @@ namespace pic_cuda {
         temp_y  =  jr_y-double(j_y);
 
         //To MPI
-        Ep_X = (1 - temp_x) * (1 - temp_y) * E_X[j_x * J_Y + j_y] +
+        if (rank == 0) {
+          Ep_X = (1 - temp_x) * (1 - temp_y) * E_X[j_x * J_Y + j_y] +
           temp_x * (1 - temp_y) * E_X[(j_x + 1) * J_Y + j_y] +
           (1 - temp_x) * temp_y * E_X[j_x * J_Y + (j_y + 1)] +
           temp_x * temp_y * E_X[(j_x + 1) * J_Y + (j_y + 1)];
-
-        Ep_Y = (1 - temp_x) * (1 - temp_y) * E_Y[j_x * J_Y + j_y] +
+        }
+        else if (rank == 1) {
+          Ep_Y = (1 - temp_x) * (1 - temp_y) * E_Y[j_x * J_Y + j_y] +
           temp_x * (1 - temp_y) * E_Y[(j_x + 1) * J_Y + j_y] +
           (1 - temp_x) * temp_y * E_Y[j_x * J_Y + (j_y + 1)] +
           temp_x * temp_y * E_Y[(j_x + 1) * J_Y + (j_y + 1)];
+          rank = 0;
+        }
 
-        vel_x[i] += DT * fact * Ep_X;
-        vel_y[i] += DT * fact * Ep_Y;
+        if (rank == 0) vel_x[i] += DT * fact * Ep_X;
+        else if (rank == 1) {
+          vel_y[i] += DT * fact * Ep_Y;
+          rank = 0;
+         }
 
-        pos_x[i] += vel_x[i] * DT;
-        pos_y[i] += vel_y[i] * DT;
+        if (rank == 0) pos_x[i] += vel_x[i] * DT;
+        else if (rank == 1) {
+          pos_y[i] += vel_y[i] * DT;
+          rank = 0;
+        }
 
         if(pos_x[i] < 0) {//Rebote en la pared del material.
           pos_x[i] = -pos_x[i];
@@ -581,10 +601,15 @@ namespace pic_cuda {
 
       if (h_pos_x[i] >= 0 && h_pos_x[i] <= L_MAX_X) {
         //To MPI
-        h_pos_x[k] = h_pos_x[i];
-        h_pos_y[k] = h_pos_y[i];
-        h_vel_x[k] = h_vel_x[i];
-        h_vel_y[k] = h_vel_y[i];
+        if (rank == 0) {
+          h_pos_x[k] = h_pos_x[i];
+          h_vel_x[k] = h_vel_x[i];
+        }
+        else if (rank == 1){
+          h_pos_y[k] = h_pos_y[i];
+          h_vel_y[k] = h_vel_y[i];
+          rank = 0;
+        }
         k++;
       }
     }
@@ -610,30 +635,43 @@ namespace pic_cuda {
 
     for (int i = 0; i < NSP; i++) {
       //To MPI
-      jr_x = pos_x[i] / hx;     // Índice (real) de la posición de la superpartícula (X)
-      j_x  = int(jr_x);        // Índice  inferior (entero) de la celda que contiene a la superpartícula (X)
-      temp_x = jr_x - double(j_x);
-      jr_y = pos_y[i] / hx;     // Índice (real) de la posición de la superpartícula (Y)
-      j_y  = int(jr_y);        // Índice  inferior (entero) de la celda que contiene a la superpartícula (Y)
-      temp_y  =  jr_y-double(j_y);
+      if (rank == 0) {  
+        jr_x = pos_x[i] / hx;     // Índice (real) de la posición de la superpartícula (X)
+        j_x  = int(jr_x);        // Índice  inferior (entero) de la celda que contiene a la superpartícula (X)
+        temp_x = jr_x - double(j_x);
+      }
+      else if (rank == 1) {
+        jr_y = pos_y[i] / hx;     // Índice (real) de la posición de la superpartícula (Y)
+        j_y  = int(jr_y);        // Índice  inferior (entero) de la celda que contiene a la superpartícula (Y)
+        temp_y  =  jr_y-double(j_y);
+        rank = 0;
+      }
 
       //To MPI
-      Ep_X = (1 - temp_x) * (1 - temp_y) * E_X[j_x * J_Y + j_y] +
+      if (rank == 0) {
+        Ep_X = (1 - temp_x) * (1 - temp_y) * E_X[j_x * J_Y + j_y] +
         temp_x * (1 - temp_y) * E_X[(j_x + 1) * J_Y + j_y] +
         (1 - temp_x) * temp_y * E_X[j_x * J_Y + (j_y + 1)] +
         temp_x * temp_y * E_X[(j_x + 1) * J_Y + (j_y + 1)];
-
-      Ep_Y = (1 - temp_x) * (1 - temp_y) * E_Y[j_x * J_Y + j_y] +
+      }
+      else if (rank == 1) {
+        Ep_Y = (1 - temp_x) * (1 - temp_y) * E_Y[j_x * J_Y + j_y] +
         temp_x * (1 - temp_y) * E_Y[(j_x + 1) * J_Y + j_y] +
         (1 - temp_x) * temp_y * E_Y[j_x * J_Y + (j_y + 1)] +
         temp_x * temp_y * E_Y[(j_x + 1) * J_Y + (j_y + 1)];
+        rank = 0;
+      }
 
       //To MPI
-      vel_x[i] = vel_x[i] + CTE_E * FACTOR_CARGA_E * fact * Ep_X * DT;
-      vel_y[i] = vel_y[i] + CTE_E * FACTOR_CARGA_E * fact * Ep_Y * DT;
-
-      pos_x[i] += vel_x[i] * DT;
-      pos_y[i] += vel_y[i] * DT;
+      if (rank == 0) {
+        vel_x[i] = vel_x[i] + CTE_E * FACTOR_CARGA_E * fact * Ep_X * DT;
+        pos_x[i] += vel_x[i] * DT;
+      }
+      else if (rank == 1) {
+        vel_y[i] = vel_y[i] + CTE_E * FACTOR_CARGA_E * fact * Ep_Y * DT;
+        pos_y[i] += vel_y[i] * DT;
+        rank = 0;
+      }
 
       if(pos_x[i] < 0) {//Rebote en la pared del material.
         pos_x[i] = -pos_x[i];
@@ -661,10 +699,15 @@ namespace pic_cuda {
 
       if(pos_x[i] >= 0 && pos_x[i] <= L_MAX_X) {
         //To MPI
-        pos_x[kk1] = pos_x[i];
-        pos_y[kk1] = pos_y[i];
-        vel_x[kk1] = vel_x[i];
-        vel_y[kk1] = vel_y[i];
+        if (rank == 0) {
+          pos_x[kk1] = pos_x[i];
+          vel_x[kk1] = vel_x[i];
+        }
+        else if (rank == 1){
+          pos_y[kk1] = pos_y[i];
+          vel_y[kk1] = vel_y[i];
+          rank = 0;
+        }
         kk1++;
       }
 
