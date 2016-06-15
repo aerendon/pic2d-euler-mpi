b1cd608a809c0fbd0b1b04b5cf5b90622f7abf50
Alejandro E. Rendon <alejorendon@utp.edu.co>
2016-06-15 13:33:18 -0500

    Pruebas - inicialize

--- secuential/motion/mpi-motion.cpp
+++ secuential/motion/mpi-motion.cpp
@@ -156,7 +156,6 @@ using namespace std;
         jr_y = pos_y[i] / hx; // indice (real) de la posición de la superpartícula
         j_y  = (int) jr_y;    // indice  inferior (entero) de la celda que contiene a la superpartícula
         temp_y  =  jr_y - j_y;
-      
 	    n[j_y + j_x * J_Y] += (1. - temp_x) * (1. - temp_y) / (hx * hx * hx);
       n[j_y + (j_x + 1) * J_Y] += temp_x * (1. - temp_y) / (hx * hx * hx);
      	n[(j_y + 1) + j_x * J_Y] += (1. - temp_x) * temp_y / (hx * hx * hx);
